7af790e926d7385d2d5cffbfcb4e583fab0cbfad
Evgeny Leshchyov <Evgeny.Leshchyov@wrenkitchens.com>
2018-05-23 00:23:00 +0300

    implement localization

---
+++ WeatherApp/WeatherApp/Localization/ILocalize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace WeatherApp.Localization
+{    
+    public interface ILocalize
+    {
+        ///	<summary>
+        /// This method must evaluate platform-specific locale settings
+        /// and convert them (when necessary) to a valid .NET locale.
+        /// </summary>
+        CultureInfo GetCurrentCultureInfo();
+
+        /// <summary>
+        /// CurrentCulture and CurrentUICulture must be set in the platform project, 
+        /// because the Thread object can't be accessed in a PCL.
+        /// </summary>
+        void SetLocale(CultureInfo ci);
+    }    
+}
---
+++ WeatherApp/WeatherApp/Localization/TranslateExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp.Localization
+{    
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci = null;
+        const string ResourceId = "WeatherApp.Resx.AppResources";
+
+        static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(ResourceId, IntrospectionExtensions.GetTypeInfo(typeof(TranslateExtension)).Assembly));
+
+        public string Text { get; set; }
+
+        public TranslateExtension()
+        {
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                ILocalize nativeImpl = null;
+                nativeImpl = DependencyService.Get<ILocalize>();
+                ci = nativeImpl.GetCurrentCultureInfo();
+            }
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+            {
+                return string.Empty;
+            }
+
+            var translation = ResMgr.Value.GetString(Text, ci);
+            if (translation == null)
+            {
+                throw new ArgumentException(
+                    string.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
+                    "Text");
+            }
+
+            return translation;
+        }
+    }
+}
