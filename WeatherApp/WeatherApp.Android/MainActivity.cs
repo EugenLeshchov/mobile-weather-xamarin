66554f18a294b6ed4d6cb4f392cabb3beeca8ced
Evgeny Leshchyov <Evgeny.Leshchyov@wrenkitchens.com>
2018-05-23 22:05:59 +0300

    fixes

--- WeatherApp/WeatherApp.Android/MainActivity.cs
+++ WeatherApp/WeatherApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using WeatherApp.Views;
 
 namespace WeatherApp.Droid
 {
@@ -20,6 +21,8 @@ namespace WeatherApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
+
             LoadApplication(new App());
         }
     }
---
+++ WeatherApp/WeatherApp.Android/Renderers/CustomMultiLineLabelRenderer.cs
@@ -0,0 +1,27 @@
+﻿using WeatherApp.Controls;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(MultiLineLabel), typeof(WeatherApp.Droid.Renderers.CustomMultiLineLabelRenderer))]
+namespace WeatherApp.Droid.Renderers
+{
+    using Android.Content;
+    using Xamarin.Forms.Platform.Android;
+
+    public class CustomMultiLineLabelRenderer : LabelRenderer
+    {
+        public CustomMultiLineLabelRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            MultiLineLabel multiLineLabel = (MultiLineLabel)Element;
+
+            if (multiLineLabel != null && multiLineLabel.Lines != -1)
+            {
+                Control.SetSingleLine(false);
+                Control.SetLines(multiLineLabel.Lines);
+            }
+        }
+    }
+}
